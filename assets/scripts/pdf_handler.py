bb53577636d9ea787e8bd13de1b71a9e2fc56f21
srabon <totalblank@yahoo.com>
2024-05-04 00:40:42 +0600

    A lot changed

--- assets/scripts/pdf_handler.py
+++ assets/scripts/pdf_handler.py
@@ -3,14 +3,15 @@ import os, shutil
 list_pdfs = []
 
 for root, dirs, files in os.walk('../../'):
-    for file in files:
-        if os.path.splitext(file)[1] == '.pdf':
-            list_pdfs.append(os.path.abspath(os.path.join(root, file)))
+    if not (root.startswith('../../assets') and root.startswith('../../pdf_exports')):
+        for file in files:
+            if os.path.splitext(file)[1] == '.pdf':
+                list_pdfs.append(os.path.abspath(os.path.join(root, file)))
 
 list_pdfs = list(set(list_pdfs))
 
 for file in list_pdfs:
     try:
-        shutil.move(file, f'../../pdf_exports/{os.path.split(file)[1]}')
-    except FileNotFoundError:
-        print("File not found")
+        shutil.copy(file, f'../../pdf_exports/{os.path.split(file)[1]}')
+    except (FileNotFoundError, shutil.SameFileError) as error:
+        print(error)
